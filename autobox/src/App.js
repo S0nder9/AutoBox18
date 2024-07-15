10a322c7fc9216a5f0b691ca42f604a4305e6b3f
S0nder9 <n.patrushev9@gmail.com>
2024-07-15 09:06:50 +0400

    Update

--- autobox/src/App.js
+++ autobox/src/App.js
@@ -1,44 +1,5 @@
-// import React from "react";
-// import { Route, Redirect, Switch } from "react-router-dom";
-// import Main from "./Pages/Main";
-// import PricesAndServices from "./Pages/PricesAndServices";
-// import WindowPage from "./Pages/WindowPage";
-// import Contacts from "./Pages/Contacts";
-// import PrivacyPolicy from "./Pages/privacyPolicy";
-// import NotFound from "./Pages/NotFound";
-
-// function App() {
-//   return (
-//     <Switch>
-//       <Route exact path="/">
-//         <Redirect to="/main" />
-//       </Route>
-//       <Route path="/main">
-//         <Main />
-//       </Route>
-//       <Route path="/pricesAndServices" exact>
-//         <PricesAndServices />
-//       </Route>
-//       <Route path="/contacts" exact>
-//         <Contacts />
-//       </Route>
-//       <Route path="/privacyPolicy" exact>
-//         <PrivacyPolicy />
-//       </Route>
-//       <Route path="/pricesAndServices/:id">
-//         <WindowPage />
-//       </Route>
-//       <Route path="*">
-//        <NotFound/>
-//       </Route>
-//     </Switch>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Main from "./Pages/Main";
 import PricesAndServices from "./Pages/PricesAndServices";
 import WindowPage from "./Pages/WindowPage";
@@ -48,31 +9,29 @@ import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
-    <Router basename="/AutoBox18/autobox/public">
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/main" />
-        </Route>
-        <Route path="/main" exact>
-          <Main />
-        </Route>
-        <Route path="/pricesAndServices" exact>
-          <PricesAndServices />
-        </Route>
-        <Route path="/contacts" exact>
-          <Contacts />
-        </Route>
-        <Route path="/privacyPolicy" exact>
-          <PrivacyPolicy />
-        </Route>
-        <Route path="/pricesAndServices/:id">
-          <WindowPage />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/main" />
+      </Route>
+      <Route path="/main">
+        <Main />
+      </Route>
+      <Route path="/pricesAndServices" exact>
+        <PricesAndServices />
+      </Route>
+      <Route path="/contacts" exact>
+        <Contacts />
+      </Route>
+      <Route path="/privacyPolicy" exact>
+        <PrivacyPolicy />
+      </Route>
+      <Route path="/pricesAndServices/:id">
+        <WindowPage />
+      </Route>
+      <Route path="*">
+       <NotFound/>
+      </Route>
+    </Switch>
   );
 }
 
