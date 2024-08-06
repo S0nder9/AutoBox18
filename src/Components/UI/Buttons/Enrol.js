5758f16adb5a11ce6b2ecf8e636a69d3aa3cd50c
S0nder9 <n.patrushev9@gmail.com>
2024-08-06 09:50:09 +0400

    Update

--- src/Components/UI/Buttons/Enrol.js
+++ src/Components/UI/Buttons/Enrol.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./Enrol.module.css";
-import { entrySliceClose, entrySliceOpen } from "../../../Store/Slices/entrySlice";
+import {
+    entrySliceClose,
+    entrySliceOpen,
+} from "../../../Store/Slices/entrySlice";
 import Modal from "../Modal/Modal";
 import BcModal from "../Modal/BcModal";
 
 const Enrol = (props) => {
     const [anim, setAnim] = useState(false);
-    const openModalWindow = useSelector(state => state.entrySlice.entrySliceOpenState);
+    const openModalWindow = useSelector(
+        (state) => state.entrySlice.entrySliceOpenState
+    );
     const dispatch = useDispatch();
-    
 
     const animationActivationHandler = () => {
         setAnim(true);
@@ -17,11 +21,14 @@ const Enrol = (props) => {
 
     return (
         <>
-            <button className={styles.button} onClick={() => dispatch(entrySliceOpen())}>
+            <button
+                className={styles.button}
+                onClick={() => dispatch(entrySliceOpen())}
+            >
                 {props.text}
             </button>
             {openModalWindow && (
-                <Modal onClose={() => dispatch(entrySliceClose())} >
+                <Modal onClose={() => dispatch(entrySliceClose())}>
                     <BcModal onClick={animationActivationHandler} />
                 </Modal>
             )}
--- src/Components/UI/Footer/Footer.js
+++ src/Components/UI/Footer/Footer.js
@@ -1,7 +1,7 @@
 import SocialIcons from "../../Icons/SocialIcons";
 import Enrol from "../Buttons/Enrol";
 import styles from "./Footer.module.css";
-import Line from "../Line/Line"
+import Line from "../Line/Line";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import useExchangeBuffer from "../../../Hooks/useExchangeBuffer";
 import SuccessPopup from "../ErrorGroup/SuccessPopup";
@@ -9,12 +9,17 @@ import ErrorPopup from "../ErrorGroup/ErrorPopup";
 import obj_with_constants from "../../../Helper/constants";
 
 const Footer = () => {
-    const { handleCopy, textSuccess, textError, forPopupState } = useExchangeBuffer();
+    const { handleCopy, textSuccess, textError, forPopupState } =
+        useExchangeBuffer();
 
     return (
         <>
-            {forPopupState && <SuccessPopup message={textSuccess} timeOut="5000" />}
-            {forPopupState === false && <ErrorPopup message={textError} timeOut="5000" />}
+            {forPopupState && (
+                <SuccessPopup message={textSuccess} timeOut="5000" />
+            )}
+            {forPopupState === false && (
+                <ErrorPopup message={textError} timeOut="5000" />
+            )}
 
             <div className={styles.entry}>
                 <Enrol text="ЗАПИСЬ ОНЛАЙН" />
@@ -22,7 +27,9 @@ const Footer = () => {
             </div>
             <div className={styles.footerContainer}>
                 <div className={styles.firstContainerForFooter}>
-                    <Link to="/main"><p className="textWhiteBig">АВТОБОКС18</p></Link>
+                    <Link to="/main">
+                        <p className="textWhiteBig">АВТОБОКС18</p>
+                    </Link>
                     <br />
                     <SocialIcons justifyContent="space-around" />
                 </div>
@@ -32,39 +39,41 @@ const Footer = () => {
                     <br />
                     <Link to="/pricesAndServices">
                         <div className={styles.crossWhithText}>
-                            <box-icon name='x' color="#FF5C00" />
+                            <box-icon name="x" color="#FF5C00" />
                             <p className="textWhiteSmall">ЦЕНЫ И УСЛУГИ</p>
                         </div>
                     </Link>
                     <div className={styles.crossWhithText}>
-                        <box-icon name='x' color="#FF5C00" />
+                        <box-icon name="x" color="#FF5C00" />
                         <p className="textWhiteSmall">ЗАПЧАСТИ</p>
                     </div>
                     <Link to="/contacts">
                         <div className={styles.crossWhithText}>
-                            <box-icon name='x' color="#FF5C00" />
+                            <box-icon name="x" color="#FF5C00" />
                             <p className="textWhiteSmall">КОНТАКТЫ</p>
                         </div>
                     </Link>
                 </div>
 
                 <div className={styles.thirdContainerForFooter}>
-                    <p className="textWhiteBig" style={{textAlign: "center"}}>ИНФОРМАЦИЯ</p>
+                    <p className="textWhiteBig" style={{ textAlign: "center" }}>
+                        ИНФОРМАЦИЯ
+                    </p>
                     <br />
                     <div className={styles.crossWhithText} onClick={handleCopy}>
-                        <box-icon name='x' color="#FF5C00" />
+                        <box-icon name="x" color="#FF5C00" />
                         <p className="textWhiteSmall">{`АДРЕС: ${obj_with_constants.ADDRESS}`}</p>
                     </div>
                     <div className={styles.crossWhithText} onClick={handleCopy}>
-                        <box-icon name='x' color="#FF5C00" />
+                        <box-icon name="x" color="#FF5C00" />
                         <p className="textWhiteSmall">{`ТЕЛЕФОН: ${obj_with_constants.PHONENUMBER1}, ${obj_with_constants.PHONENUMBER2}`}</p>
                     </div>
                     <div className={styles.crossWhithText} onClick={handleCopy}>
-                        <box-icon name='x' color="#FF5C00" />
+                        <box-icon name="x" color="#FF5C00" />
                         <p className="textWhiteSmall">{`ВРЕМЯ РАБОТЫ: ${obj_with_constants.SCHEDULE}`}</p>
                     </div>
                     <div className={styles.crossWhithText} onClick={handleCopy}>
-                        <box-icon name='x' color="#FF5C00" />
+                        <box-icon name="x" color="#FF5C00" />
                         <p className="textWhiteSmall">{`ПОЧТА: ${obj_with_constants.MAIL}`}</p>
                     </div>
                 </div>
